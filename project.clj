3a596d4926908dd6dc4223f2183d73b447d26efd
John Szakmeister <john@szakmeister.net>
2012-05-08 16:27:36 -0400

    Change the version number, so we can continue to work.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-anti-forgery "0.1.2"
+(defproject ring-anti-forgery "0.1.2-szak"
   :description "Ring middleware to prevent CSRF attacks"
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :profiles
